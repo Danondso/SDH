30487f0d8217d8e14b198c5517caf5a3d0ad9ec2
megoff <megoff@uark.edu>
2014-04-10 22:27:34 -0500

    Filled out the ItemPool class

--- Saving Duck Hurlington/src/JohnnyComeLately/ItemPool.java
+++ Saving Duck Hurlington/src/JohnnyComeLately/ItemPool.java
@@ -1,11 +1,30 @@
 package JohnnyComeLately;
 
-public class ItemPool {
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Random;
 
+public class ItemPool implements Comparator<ItemPoolNode>{
+	private PriorityQueue<ItemPoolNode> Pool;
+	
+	public ItemPool(){
+		Random rand = new Random();
+		Pool.add(new ItemPoolNode(new PanicBoots(new Position(250, 250)), rand.nextDouble()));
+		Pool.add(new ItemPoolNode(new PanicGloves(new Position(250, 250)), rand.nextDouble()));
+	}
 
+	public Item NextItem(){
+		return (Pool.poll()).GetItem();
+	}
 
-public Item NextItem(){
-	return new Item();
-}
-
+	@Override
+	public int compare(ItemPoolNode node1, ItemPoolNode node2) {
+		if(node1.GetValue() < node2.GetValue())
+			return -1;
+			
+		if(node1.GetValue() > node2.GetValue())
+			return 1;
+		
+		return 0;
+	}
 }

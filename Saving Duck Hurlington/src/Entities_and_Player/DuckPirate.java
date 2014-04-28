2bec9545d8c03d225de9e87e0a18f5ce09eb41a9
megoff <megoff@uark.edu>
2014-04-27 21:26:26 -0500

    Fixes to damage on Duck Pirate and Sea turtle
    Duck Pirate doesn't shoot correctly, he angling requires some figuring
    out.

--- Saving Duck Hurlington/src/Entities_and_Player/DuckPirate.java
+++ Saving Duck Hurlington/src/Entities_and_Player/DuckPirate.java
@@ -6,13 +6,17 @@ public class DuckPirate extends Creature{
 		public DuckPirate(Position p, Player player)
 		{
 		//figure out how to slow him, too OP
-	    super(30, 30, 10, 1, 0, p, "/Entities/Enemies/DuckPirate.png");
+	    super(30, 30, 1, 1, 0, p, "/Entities/Enemies/DuckPirate.png");
 		 Player = player;
 		 Position = p;
 	 }
 
-		Projectile AttackMethod() {
-			Sword sword = new Sword(2, 2, 2, 2, Enemy, new Position(Position.X, Position.Y));
+		Projectile AttackMethod() {			
+			int angle = (int) Math.atan2(Player.Position.GetX() - Position.GetX(), Player.Position.GetY() - Position.GetY());
+			int xspeed = (int) (ShotSpeed*Math.cos(angle));
+			int yspeed = (int) (ShotSpeed*Math.sin(angle));
+			Sword sword = new Sword(2, xspeed, yspeed, 20, Enemy, new Position(Position.X, Position.Y));
+
 			return sword;
 		}
 		
